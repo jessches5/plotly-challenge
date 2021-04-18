39683b71f5c1f5028b9964256a139c6ab8859c38
Jessica Chester <chesterjessica10@gmail.com>
2021-04-17 22:47:02 -0500

    set promise pending

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,11 @@
 // Create a function for the dropdown menu
 function chooseID(chosenID) {
     console.log(chosenID);
-}
+
+    d3.json("./samples.json").then((data) => {
+        console.log(data);
+    });
+
+    // Promise Pending
+    const dataPromise = d3.json("./samples.json");
+    console.log("Data Promise: ", dataPromise);};
